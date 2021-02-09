466fed029cf6614439cd33d3b6e8557030040e16
naveennvm4 <naveennvm4@gmail.com>
2021-02-09 10:59:48 +0530

    Refactor: by using maven project

--- Helloworld.java
+++ Helloworld.java
@@ -1,5 +1,23 @@
-class Helloworld {
-	public static void main(String args[]){
-	System.out.println("hellojava");
-}
+package com.javapractice;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	private static final Logger LOG = LogManager.getLogger(App.class);
+    public static void main( String[] args )
+    {
+       String message = "Hello world";
+       LOG.debug(message + "Will be Printed on debug");
+       LOG.info(message + "Will be Printed on info");
+       LOG.warn(message + "Will be Printed on warn");
+       LOG.error(message + "Will be Printed on error");
+       LOG.fatal(message + "Will be Printed on fatal");
+       LOG.info("Appending String: {}.", message);
+       System.out.println(message);
+    }
 }
